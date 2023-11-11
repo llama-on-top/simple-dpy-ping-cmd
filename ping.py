bf89cf9650bc1a46399ddfdc534a980a9c710bef
llama-on-top <150541633+llama-on-top@users.noreply.github.com>
2023-11-11 18:21:30 +0000

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,21 @@
+# Imports
+import discord
+from discord.ext import commands
+
+# Intents + Prefix
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='p!', intents=intents)
+
+# When bot is ready
+@bot.event
+async def on_ready():
+    print(f'Ping command is ready to be used!')
+
+
+# Ping command
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+# Bot token
+bot.run('YOUR_BOT_TOKEN')
